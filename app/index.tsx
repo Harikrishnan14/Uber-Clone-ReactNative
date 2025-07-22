3ba04fa9fc961643872ae61cbcafedc759ec5071
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-07-22 15:17:40 +0530

    Update: redux setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
+import { store } from "@/redux/store";
 import { Text, View } from "react-native";
+import { Provider } from 'react-redux'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Provider store={store}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </Provider>
   );
 }
